6611390ed2bb619463273183b44be48c9fbf775d
Zak Smith <zsmith3.dev@gmail.com>
2021-07-05 13:51:09 +0100

    Add king moves

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -6,6 +6,8 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const allMoves = this.getLateralMoves(board).concat(this.getDiagonalMoves(board));
+        return allMoves.filter(square => square.isAdjacentTo(currentSquare))
     }
 }
--- src/engine/square.js
+++ src/engine/square.js
@@ -22,6 +22,12 @@ export default class Square {
         return this.row >= 0 && this.row < GameSettings.BOARD_SIZE && this.col >= 0 && this.col < GameSettings.BOARD_SIZE;
     }
 
+    isAdjacentTo(otherSquare) {
+        const rowDiff = Math.abs(this.row - otherSquare.row);
+        const colDiff = Math.abs(this.col - otherSquare.col);
+        return rowDiff <= 1 && colDiff <= 1 && rowDiff + colDiff > 0;
+    }
+
     toString() {
         return `Row ${this.row}, Col ${this.col}`;
     }

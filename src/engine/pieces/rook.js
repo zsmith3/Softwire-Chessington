491a783bb339e2ba263abef3dfc50f23b37fe8e3
Zak Smith <zsmith3.dev@gmail.com>
2021-07-05 12:17:46 +0100

    Add rook available moves

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,11 +1,25 @@
 import Piece from './piece';
+import GameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
     constructor(player) {
         super(player);
     }
 
+    getHorizontalMoves(currentSquare) {
+        const cols = [...Array(GameSettings.BOARD_SIZE).keys()].filter(i => i !== currentSquare.col);
+        return cols.map(col => ({row: currentSquare.row, col: col}));
+    }
+
+    getVerticalMoves(currentSquare) {
+        const rows = [...Array(GameSettings.BOARD_SIZE).keys()].filter(i => i !== currentSquare.row);
+        return rows.map(row => ({row: row, col:  currentSquare.col}));
+    }
+
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const horizontalMoves = this.getHorizontalMoves(currentSquare);
+        const verticalMoves = this.getVerticalMoves(currentSquare);
+        return horizontalMoves.concat(verticalMoves);
     }
 }

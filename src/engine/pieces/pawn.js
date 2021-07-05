96f4dc6997cbc386e15bc92563b6503e711c543e
Zak Smith <zsmith3.dev@gmail.com>
2021-07-05 14:12:55 +0100

    Stop pawns leaving board

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,5 +1,6 @@
 import Piece from './piece';
 import Player from "../player";
+import GameSettings from "../gameSettings";
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -10,8 +11,11 @@ export default class Pawn extends Piece {
         const currentSquare = board.findPiece(this);
         const direction = this.player === Player.WHITE ? 1 : -1;
         const isFirstMove = (this.player === Player.WHITE && currentSquare.row === 1) || (this.player === Player.BLACK && currentSquare.row === 6);
+        const newRow = currentSquare.row + direction;
+        const canMove = newRow >= 0 && newRow < GameSettings.BOARD_SIZE;
         if (isFirstMove) return [currentSquare.moveBy(direction, 0), currentSquare.moveBy(2 * direction, 0)];
-        else return [currentSquare.moveBy(direction, 0)];
+        else if (canMove) return [currentSquare.moveBy(direction, 0)];
+        else return [];
     }
 
     getAvailableMoves(board) {
